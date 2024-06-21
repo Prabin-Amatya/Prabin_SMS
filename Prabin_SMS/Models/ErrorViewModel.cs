66197d444bb1aa30b366e993bbe457aec970ce1f
Prabin Amatya <110387564+Prabin-Amatya@users.noreply.github.com>
2024-06-21 20:23:00 +0545

    Delete Prabin_SMS directory

--- Prabin_SMS/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Prabin_SMS.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
